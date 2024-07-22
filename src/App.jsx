b5801e4e78ea4a09c5a7931b0ddf65170234952d
to4sn1311 <vutoan131102@gmail.com>
2024-07-23 03:34:09 +0700

    header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function App() {
-  return <p className="font-bold text-lg">NETFLIX</p>;
+  return (
+    <div>
+      <header className="flex h-14 items-center justify-between bg-slate-950 px-8 text-white">
+        <div className="flex items-center gap-4">
+          <img className="w-16 sm:w-28" src="./netflix.png" alt="" />
+          <a href="">Movies</a>
+          <a href="">TV Shows</a>
+        </div>
+        <div>
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="cursor-pointer"
+          />
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
